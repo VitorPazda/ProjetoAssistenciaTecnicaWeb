b4d86f0e87d84722e4944d41abc21762c02850c2
Nogrilo <nogriloesirjeferson@gmail.com>
2026-01-27 21:19:16 -0300

    Add project files.

---
+++ ProjetoAssistenciaTecnicaWeb/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoAssistenciaTecnicaWeb.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF_CNPJ {get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Modalidade { get; set; }
+    }
+}
