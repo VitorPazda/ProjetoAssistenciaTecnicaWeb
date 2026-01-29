1e2b946e309f89dac45efb651730dd39e193adb2
Nogrilo <nogriloesirjeferson@gmail.com>
2026-01-29 18:58:08 -0300

    atualizando o lixo da classe cliente

--- ProjetoAssistenciaTecnicaWeb/Models/Cliente.cs
+++ ProjetoAssistenciaTecnicaWeb/Models/Cliente.cs
@@ -1,7 +1,10 @@
-﻿namespace ProjetoAssistenciaTecnicaWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAssistenciaTecnicaWeb.Models
 {
     public class Cliente
     {
+        [Key]
         public int IdCliente { get; set; }
         public string Nome { get; set; }
         public string CPF_CNPJ {get; set; }
