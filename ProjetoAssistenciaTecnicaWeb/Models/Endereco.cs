e0831b4983c7f75227a2b4282f10718842282d68
Nogrilo <nogriloesirjeferson@gmail.com>
2026-01-29 19:00:48 -0300

    criando a classe endereco

---
+++ ProjetoAssistenciaTecnicaWeb/Models/Endereco.cs
@@ -0,0 +1,32 @@
+﻿namespace ProjetoAssistenciaTecnicaWeb.Models
+{
+    public class Endereco
+    {
+        public int IdEndereco { get; set; } = 0;
+        public string Estado { get; set; } = string.Empty;
+        public string Municipio { get; set; } = string.Empty;
+        public string Cep { get; set; } = string.Empty;
+        public string Rua { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public string Complemento { get; set; } = string.Empty;
+        public string NCasa { get; set; } = string.Empty;
+        public int IdCliente { get; set; } = 0;
+
+        public Endereco()
+        { 
+        }
+
+        public Endereco(int idEndereco, string estado, string municipio, string cep, string rua, string bairro, string complemento, string nCasa, int idCliente)
+        {
+            IdEndereco = idEndereco;
+            Estado = estado;
+            Municipio = municipio;
+            Cep = cep;
+            Rua = rua;
+            Bairro = bairro;
+            Complemento = complemento;
+            NCasa = nCasa;
+            IdCliente = idCliente;
+        }
+    }
+}
