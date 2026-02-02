cfe244c79adda0ff8a84a247a5c96abc73aef5d8
Nogrilo <nogriloesirjeferson@gmail.com>
2026-02-02 19:19:33 -0300

    criando a ClientFormViewModel, para add os campos de endereco

---
+++ ProjetoAssistenciaTecnicaWeb/Models/ViewModels/ClienteFormViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoAssistenciaTecnicaWeb.Models.ViewModels
+{
+    public class ClienteFormViewModel
+    {
+        public Cliente Cliente { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
