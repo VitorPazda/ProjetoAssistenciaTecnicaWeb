eb412b9412d90eee2ebf6a8d84fdd34f19f7e0be
Nogrilo <nogriloesirjeferson@gmail.com>
2026-02-03 19:55:06 -0300

    ajustando os atributos do Cliente, deixando obrigatorios, e formatando os DataType

--- ProjetoAssistenciaTecnicaWeb/Models/Cliente.cs
+++ ProjetoAssistenciaTecnicaWeb/Models/Cliente.cs
@@ -6,12 +6,36 @@ namespace ProjetoAssistenciaTecnicaWeb.Models
     {
         [Key]
         public int IdCliente { get; set; }
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(13, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
+        [Display(Name = "CPF")]
         public string CPF_CNPJ {get; set; }
+        
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(12, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
+        [DataType(DataType.PhoneNumber)]
         public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [Display(Name = "Data Nascimento")]
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [Display(Name = "Data Cadastro")]
+        [DataType(DataType.Date)]
         public DateTime DataCadastro { get; set; }
+        
+        [Required(ErrorMessage = "{0} é obrigatório")]
         public string Modalidade { get; set; }
         public int EnderecoId { get; set; }
         public Endereco Endereco { get; set; }
