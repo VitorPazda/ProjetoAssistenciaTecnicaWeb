99c3a7b06573eb381034b955f779a97c809316fa
Nogrilo <nogriloesirjeferson@gmail.com>
2026-02-26 19:13:14 -0300

    adicionando a EnderecoService, e voltando ao localhost para o bd

---
+++ ProjetoAssistenciaTecnicaWeb/Services/EnderecoService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoAssistenciaTecnicaWeb.Data;
+using ProjetoAssistenciaTecnicaWeb.Models;
+
+namespace ProjetoAssistenciaTecnicaWeb.Services
+{
+    public class EnderecoService
+    {
+        private readonly ProjetoAssistenciaTecnicaWebContext _context;
+
+        public EnderecoService(ProjetoAssistenciaTecnicaWebContext context)
+        {
+            _context = context;
+        }
+
+        public void Insert(EnderecoService obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public async Task<List<Cliente>> FindAsyc(string nome)
+        {
+            var resultado = _context.Cliente
+                .Include(x => x.Endereco)
+                .AsQueryable();
+
+
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                // retornar os utlimos 5 clientes cadastrados
+
+                return await resultado
+                    .OrderByDescending(x => x.DataCadastro)
+                    .Take(5)
+                    .ToListAsync();
+            }
+
+            return await resultado
+                .Where(x => x.Nome.Contains(nome))
+                .ToListAsync();
+        }
+    }
+}
