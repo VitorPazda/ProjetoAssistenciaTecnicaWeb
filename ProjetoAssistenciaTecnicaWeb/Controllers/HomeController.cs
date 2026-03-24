f49f326a3cd6c1d80c129c3b6755c2bfd4556dbd
Vinicius Miranda <viniciuscorrea509@gmail.com>
2026-03-23 21:28:50 -0300

    inserção de link referencia para a pagina Funcionarios

--- ProjetoAssistenciaTecnicaWeb/Controllers/HomeController.cs
+++ ProjetoAssistenciaTecnicaWeb/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace ProjetoAssistenciaTecnicaWeb.Controllers
             return View();
         }
 
+        public IActionResult Funcionarios()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ ProjetoAssistenciaTecnicaWeb/Views/Home/Funcionarios.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoAssistenciaTecnicaWeb.Pages.Home
+{
+    public class FuncionariosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
