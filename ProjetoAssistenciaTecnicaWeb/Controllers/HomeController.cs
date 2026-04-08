dda2d2049346370f6544f61f162a4fef895b49d3
Vinicius Miranda <viniciuscorrea509@gmail.com>
2026-04-08 20:37:07 -0300

    Adicionei a pagina Ordem de Serviços

--- ProjetoAssistenciaTecnicaWeb/Controllers/HomeController.cs
+++ ProjetoAssistenciaTecnicaWeb/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace ProjetoAssistenciaTecnicaWeb.Controllers
             return View();
         }
 
+        public IActionResult OrdemServicos()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
