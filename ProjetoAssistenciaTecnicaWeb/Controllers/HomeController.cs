967de33526a0d1a6deda8ab84a2a6a111356808f
Vinicius Miranda <viniciuscorrea509@gmail.com>
2026-04-09 20:22:51 -0300

    alterando umas coisas ai

--- ProjetoAssistenciaTecnicaWeb/Controllers/HomeController.cs
+++ ProjetoAssistenciaTecnicaWeb/Controllers/HomeController.cs
@@ -39,6 +39,12 @@ namespace ProjetoAssistenciaTecnicaWeb.Controllers
             return View();
         }
 
+        public IActionResult Pecas()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ ProjetoAssistenciaTecnicaWeb/Views/Home/Pecas.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoAssistenciaTecnicaWeb.Pages.Home
+{
+    public class PecasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
