4615c650ee7c6530ab92d86ffaeda3bf09392ace
Nogrilo <nogriloesirjeferson@gmail.com>
2026-04-14 20:24:20 -0300

    programando o get de funcionario, dentro do controller

---
+++ ProjetoAssistenciaTecnicaWeb/Controllers/FuncionariosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetoAssistenciaTecnicaWeb.Data;
+using ProjetoAssistenciaTecnicaWeb.Services;
+
+namespace ProjetoAssistenciaTecnicaWeb.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        private readonly ProjetoAssistenciaTecnicaWebContext _context;
+
+        private readonly FuncionarioService _funcionarioService;
+
+        public FuncionariosController(ProjetoAssistenciaTecnicaWebContext context, FuncionarioService funcionarioService)
+        {
+            _context = context;
+            _funcionarioService = funcionarioService;
+        }
+
+        // GET: Funcionarios
+       
+        public async Task<IActionResult> Index()
+        {
+            var projetoAssistenciaTecnicaWebContext = _context.Funcionario.Include(f => f.Endereco);
+            return View(await projetoAssistenciaTecnicaWebContext.ToListAsync());
+        }
+    }
+}
