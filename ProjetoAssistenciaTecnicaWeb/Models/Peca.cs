185773e55548521eef89f2259d2748a988fb3382
Nogrilo <nogriloesirjeferson@gmail.com>
2026-04-29 08:26:37 -0300

    criando a model Peca com seus atributos

---
+++ ProjetoAssistenciaTecnicaWeb/Models/Peca.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAssistenciaTecnicaWeb.Models
+{
+    public class Peca
+    {
+        [Key]
+        public int IdPeca { get; set; }
+
+        public float CustoCompra { get; set; }
+
+        public float ValorRevenda { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public int Codigo { get; set; }
+
+        public Peca()
+        {
+        }
+
+        public Peca(int idPeca, float custoCompra, float valorRevenda, string descricao, int quantidade, int codigo)
+        {
+            IdPeca = idPeca;
+            CustoCompra = custoCompra;
+            ValorRevenda = valorRevenda;
+            Descricao = descricao;
+            Quantidade = quantidade;
+            Codigo = codigo;
+        }
+    }
+}
