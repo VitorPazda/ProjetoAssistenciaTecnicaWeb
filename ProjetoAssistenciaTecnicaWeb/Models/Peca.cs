37269a0b82998001d79339b0e348145ca627c622
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-04 15:17:14 -0300

    ajustando a classe/model de Peca, para deixar os atributos formatados

--- ProjetoAssistenciaTecnicaWeb/Models/Peca.cs
+++ ProjetoAssistenciaTecnicaWeb/Models/Peca.cs
@@ -7,12 +7,25 @@ namespace ProjetoAssistenciaTecnicaWeb.Models
         [Key]
         public int IdPeca { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [Range(0.0, 10000000.0, ErrorMessage = "{0} o tamanho deve ser entre {2} e 1")]
+        [Display(Name = "Valor de Compra")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public float ValorCompra { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [Range(0.0, 10000000.0, ErrorMessage = "{0} o tamanho deve ser entre {2} e 1")]
+        [Display(Name = "Valor de Revenda")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public float ValorRevenda { get; set; }
 
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
         public string Descricao { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [Range(0.0, 10000000.0, ErrorMessage = "{0} o tamanho deve ser entre {2} e 1")]
+        [Display(Name = "Quantidade")]
         public int Quantidade { get; set; }
 
         public int Codigo { get; set; }
