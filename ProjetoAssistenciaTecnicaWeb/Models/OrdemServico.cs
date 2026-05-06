35e1f86f511a829adf314566f17c3484bb2faa69
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-06 15:47:08 -0300

    adicionando a classe de Produto e Ordem de Servico

---
+++ ProjetoAssistenciaTecnicaWeb/Models/OrdemServico.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAssistenciaTecnicaWeb.Models
+{
+    public class OrdemServico
+    {
+        [Key]
+        public int IdOrdemServico { get; set; }
+
+        public int Tick { get; set; }
+
+        public int NumeroAtendimento { get; set; }
+
+        public string Defeito { get; set; }
+
+        public DateTime DataAbertura { get; set; }
+
+        public string Acessorios { get; set; }
+
+        public string Status { get; set; }
+
+        public int IdOrcamentoInicial { get; set; }
+
+        public int IdCliente { get; set; }
+
+        public int IdProduto { get; set; }
+        public OrdemServico()
+        {
+        }
+
+        public OrdemServico(int idOrdemServico, int tick, int numeroAtendimento, string defeito, DateTime dataAbertura, string acessorios, string status, int idOrcamentoInicial, int idProduto)
+        {
+            IdOrdemServico = idOrdemServico;
+
+        }
+    }
+}
---
+++ ProjetoAssistenciaTecnicaWeb/Models/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAssistenciaTecnicaWeb.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int IdProduto { get; set; }
+
+        public int IdCliente { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string NSerie { get; set; }
+
+        public string Tipo { get; set; }
+        
+        public string Condicao { get; set; }
+
+        public Produto()
+        {
+        }
+
+        public Produto(int idProduto, int idCliente, string marca, string modelo, string nSerie, string tipo, string condicao)
+        {
+            IdProduto = idProduto;
+            IdCliente = idCliente;
+            Marca = marca;
+            Modelo = modelo;
+            NSerie = nSerie;
+            Tipo = tipo;
+            Condicao = condicao;
+        }
+    }
+}
