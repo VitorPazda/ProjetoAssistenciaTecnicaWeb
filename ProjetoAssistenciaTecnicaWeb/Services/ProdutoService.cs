2f7320952f772938c739726caee3a653c46b612a
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-07 08:16:39 -0300

    Criando o controller e service de produto

---
+++ ProjetoAssistenciaTecnicaWeb/Services/ProdutoService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoAssistenciaTecnicaWeb.Data;
+using ProjetoAssistenciaTecnicaWeb.Models;
+using ProjetoAssistenciaTecnicaWeb.Models.ViewModels;
+
+namespace ProjetoAssistenciaTecnicaWeb.Services
+{
+    public class ProdutoService
+    {
+        private readonly ProjetoAssistenciaTecnicaWebContext _context;
+
+        public ProdutoService(ProjetoAssistenciaTecnicaWebContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Produto>> FindAllAsync()
+        {
+            return await _context.Produto.Include(p => p.Cliente).ToListAsync();
+        }
+
+        public async Task InsertAsync(Produto produto)
+        {
+            //Salva o endereco primeiro
+            _context.Produto.Add(produto);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
