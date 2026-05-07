1eedd9f96b8b4e476dbc6e13d1cf9698829c02c1
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-07 17:29:53 -0300

    ajustando a model

---
+++ ProjetoAssistenciaTecnicaWeb/Models/OrdemServico.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAssistenciaTecnicaWeb.Models
+{
+    public class OrdemServico
+    {
+        [Key]
+        public int IdOrdemServico { get; set; }
+        public int Tick { get; set; }
+        public int NumeroAtendimento { get; set; }
+        public string Defeito { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public string Acessorios { get; set; }
+        public string Status { get; set; }
+        public int IdOrcamentoInicial { get; set; }
+        public int IdPertencente { get; set; }
+        public Produto Produto { get; set; }q
+        public int ProdutoId { get; set; }
+
+        public OrdemServico()
+        {
+        }
+
+        public OrdemServico(int tick, int numeroAtendimento, string defeito, DateTime dataAbertura, string acessorios, string status, int idOrcamentoInicial, int idPertencente, Produto produto)
+        {
+            Tick = tick;
+            NumeroAtendimento = numeroAtendimento;
+            Defeito = defeito;
+            DataAbertura = dataAbertura;
+            Acessorios = acessorios;
+            Status = status;
+            IdOrcamentoInicial = idOrcamentoInicial;
+            IdPertencente = idPertencente;
+            Produto = produto;
+        }
+    }
+}
