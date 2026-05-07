b240a9dcb22c90ddc387c2031c8fe272afdc5987
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-07 08:27:25 -0300

    Atualizando todo o ProdutoService, e com certeza precisará de correções

--- ProjetoAssistenciaTecnicaWeb/Services/ProdutoService.cs
+++ ProjetoAssistenciaTecnicaWeb/Services/ProdutoService.cs
@@ -25,5 +25,60 @@ namespace ProjetoAssistenciaTecnicaWeb.Services
             _context.Produto.Add(produto);
             await _context.SaveChangesAsync();
         }
+
+        public async Task<Produto> FindByIdAsync(int id)
+        {
+            return await _context.Produto.Include(p => p.Cliente).FirstOrDefaultAsync(p => p.IdProduto == id);
+        }
+
+        public async Task<List<Produto>> FindAsync(string modelo)
+        {
+            var resultado = _context.Produto
+                .Include(p => p.Cliente)
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(modelo))
+            {
+                resultado = resultado.Where(p => p.Modelo.Contains(modelo));
+            }
+
+            return await resultado.ToListAsync();
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            try
+            {
+                var produto = await FindByIdAsync(id);
+
+                _context.Produto.Remove(produto);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new Exception("Can not delete", e);
+            }
+        }
+
+        public async Task UpdateAsync(Produto obj)
+        {
+            bool hasAny = await _context.Produto.AnyAsync(p => p.IdProduto == obj.IdProduto);
+
+            if (!hasAny)
+            {
+                throw new ApplicationException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
