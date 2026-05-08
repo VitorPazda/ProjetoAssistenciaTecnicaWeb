68a6251d0fc3e4eaa0b521146fc0b73da52df775
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-08 09:10:17 -0300

    Alterando os atributos de Produto, add Data Annotations

--- ProjetoAssistenciaTecnicaWeb/Models/Produto.cs
+++ ProjetoAssistenciaTecnicaWeb/Models/Produto.cs
@@ -9,18 +9,22 @@ namespace ProjetoAssistenciaTecnicaWeb.Models
 
         [Required(ErrorMessage = "{0} é obrigatório")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
+        [DataType(DataType.Text)]
         public string Marca { get; set; }
 
         [Required(ErrorMessage = "{0} é obrigatório")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
+        [DataType(DataType.Text)]
         public string Modelo { get; set; }
 
         [Required(ErrorMessage = "{0} é obrigatório")]
         [StringLength(100, MinimumLength = 1, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
+        [DataType(DataType.Text)]
         public string NSerie { get; set; }
 
         [Required(ErrorMessage = "{0} é obrigatório")]
         [StringLength(200, MinimumLength = 3, ErrorMessage = "{0} o tamanho deve ser entre {2} e {1}")]
+        [DataType(DataType.Text)]
         public string Condicao { get; set; }
 
         public Cliente Cliente { get; set; }
