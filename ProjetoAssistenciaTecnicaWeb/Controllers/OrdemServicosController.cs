b139a9c7ff1ec98a7efaad3c4798db989cccc770
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-08 10:53:38 -0300

    Add todas as funcoes do controller de OrdemServico

--- ProjetoAssistenciaTecnicaWeb/Controllers/OrdemServicosController.cs
+++ ProjetoAssistenciaTecnicaWeb/Controllers/OrdemServicosController.cs
@@ -9,9 +9,156 @@ namespace ProjetoAssistenciaTecnicaWeb.Controllers
 {
     public class OrdemServicosController : Controller
     {
-        public IActionResult Index()
+        private readonly OrdemServicoService _ordemServicoService;
+        private readonly ProdutoService _produtoService;
+        private readonly ClienteService _clienteService;
+        public OrdemServicosController(OrdemServicoService ordemServicoService, ProdutoService produtoService, ClienteService clienteService)
         {
-            return View();
+            _ordemServicoService = ordemServicoService;
+            _produtoService = produtoService;
+            _clienteService = clienteService;
+        }
+
+        // GET: OrdemServicos
+        public async Task<IActionResult> Index()
+        {
+            var ordemServico = await _ordemServicoService.FindAllAsync();
+            return View(ordemServico);
+        }
+
+        // GET: OrdemServicos/Create
+        public async Task<IActionResult> Create()
+        {
+            var clientes = await _clienteService.FindAllAsync();
+            var produtos = await _produtoService.FindAllAsync();
+
+            var viewModel = new OrdemServicoFormViewModel { OrdemServico = new OrdemServico(), Clientes = clientes, Produtos = produtos };
+            return View(viewModel);
+        }
+
+        // POST: OrdemServicos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(OrdemServico ordemServico)
+        {
+            await _ordemServicoService.InsertAsync(ordemServico);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: OrdemServicos/Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+            }
+
+            var ordemServico = await _ordemServicoService.FindByIdAsync(id.Value);
+
+            if (ordemServico == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+            }
+
+            return View(ordemServico);
+        }
+
+        // POST: OrdemServicos/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ordemServico = await _ordemServicoService.FindByIdAsync(id);
+
+            if (ordemServico != null)
+            {
+                await _produtoService.RemoveAsync(id);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: OrdemServicos/Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+            }
+
+            var ordemServico = await _ordemServicoService.FindByIdAsync(id.Value);
+
+            if (ordemServico == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+            }
+
+            var viewModel = new OrdemServicoFormViewModel { OrdemServico = ordemServico };
+
+            return View(viewModel);
+        }
+
+        // GET: OrdemServicos/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+            }
+
+            var ordemServico = await _ordemServicoService.FindByIdAsync(id.Value);
+
+            if (ordemServico == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+            }
+
+            var clientes = await _clienteService.FindAllAsync();
+            var produtos = await _produtoService.FindAllAsync();
+
+            var viewModel = new OrdemServicoFormViewModel { OrdemServico = ordemServico, Produtos = produtos, Clientes = clientes };
+
+            return View(viewModel);
+        }
+
+        // POST: OrdemServicos/Edit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, OrdemServicoFormViewModel viewModel)
+        {
+            if (id != viewModel.OrdemServico.IdOrdemServico)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not mismatch" });
+            }
+            try
+            {
+                await _ordemServicoService.UpdateAsync(viewModel.OrdemServico);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ApplicationException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message }); ;
+            }
+        }
+
+        public async Task<IActionResult> Find(int numeroAtendimento, DateTime dataAbertura)
+        {
+            var resultado = await _ordemServicoService.FindAsync(numeroAtendimento, dataAbertura);
+            return View(resultado);
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
--- ProjetoAssistenciaTecnicaWeb/Models/ViewModels/OrdemServicoFormViewModel.cs
+++ ProjetoAssistenciaTecnicaWeb/Models/ViewModels/OrdemServicoFormViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class OrdemServicoFormViewModel
     {
+        public OrdemServico OrdemServico { get; set; }
         public ICollection<Cliente> Clientes { get; set; }
         public ICollection<Produto> Produtos { get; set; }
     }
