ba3fd21e62dc461461c94bff866da5d8ef958b60
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-08 08:11:08 -0300

    Criando controller e service de OrdemServico

---
+++ ProjetoAssistenciaTecnicaWeb/Controllers/OrdemServicosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoAssistenciaTecnicaWeb.Data;
+using ProjetoAssistenciaTecnicaWeb.Models;
+using ProjetoAssistenciaTecnicaWeb.Models.ViewModels;
+using ProjetoAssistenciaTecnicaWeb.Services;
+using System.Diagnostics;
+
+namespace ProjetoAssistenciaTecnicaWeb.Controllers
+{
+    public class OrdemServicosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoAssistenciaTecnicaWeb/Services/OrdemServicoService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoAssistenciaTecnicaWeb.Data;
+using ProjetoAssistenciaTecnicaWeb.Models;
+using ProjetoAssistenciaTecnicaWeb.Models.ViewModels;
+
+namespace ProjetoAssistenciaTecnicaWeb.Services
+{
+    public class OrdemServicoService
+    {
+        private readonly ProjetoAssistenciaTecnicaWebContext _context;
+
+        public OrdemServicoService(ProjetoAssistenciaTecnicaWebContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<OrdemServico>> FindAllAsync()
+        {
+            return await _context.OrdemServico.Include(o => o.Cliente).ToListAsync();
+        }
+    }
+}
