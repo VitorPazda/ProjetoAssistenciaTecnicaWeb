fb3c94f62c20e2539ea4978e96557c252ec1b632
Nogrilo <nogriloesirjeferson@gmail.com>
2026-05-11 17:14:13 -0300

    Criando a model Orcamento

---
+++ ProjetoAssistenciaTecnicaWeb/Models/Orcamento.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAssistenciaTecnicaWeb.Models
+{
+    public class Orcamento
+    {
+        [Key]
+        public int IdOrcamento { get; set; }
+
+        public string CodigoOrcamento { get; set; }
+        
+        public double Valor {  get; set; }
+        public Orcamento()
+        {
+        }
+
+        public Orcamento(int idOrcamento, string codigoOrcamento, double valor)
+        {
+            IdOrcamento = idOrcamento;
+            CodigoOrcamento = codigoOrcamento;
+            Valor = valor;
+        }
+    }
+}
