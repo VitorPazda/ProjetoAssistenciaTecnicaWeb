a6c2b5e8a9e69ad9a5bc4021f5ef66ef9f0935e2
Nogrilo <nogriloesirjeferson@gmail.com>
2026-06-05 22:42:13 -0300

    add a classe de usuario

---
+++ ProjetoAssistenciaTecnicaWeb/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoAssistenciaTecnicaWeb.Migrations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAssistenciaTecnicaWeb.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+        [Required]
+        public string Cpf { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        public int FuncionarioId { get; set; }
+        public Funcionario Funcionario { get; set; }
+        public Usuario()
+        {
+        }
+
+        public Usuario(int idUsuario, string cpf, string senha, Funcionario funcionario)
+        {
+            IdUsuario = idUsuario;
+            Cpf = cpf;
+            Senha = senha;
+            Funcionario = funcionario;
+        }
+    }
+}
